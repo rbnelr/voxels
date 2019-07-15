b8786a76d0d7aa0798cb03623affe7ab4a1fb208
Anon Ymous <rbnelr@gmx.de>
2019-07-15 11:37:24 +0200

    WIP

--- Assets/Prototyping/OctreeGeneration/TerrainNode.cs
+++ Assets/Prototyping/OctreeGeneration/TerrainNode.cs
@@ -22,24 +22,72 @@ namespace OctreeGeneration {
 		}
 	}
 
+	/// <summary>
+	/// Used to uniquely identify octree nodes
+	/// </summary>
+	public struct OctreeCoord : System.IEquatable<OctreeCoord> {
+		public readonly int		lod;
+		public readonly int3	pos; // in voxel space, ie. worldPos / VoxelSize
+		
+		public int3	size => TerrainNode.VOXEL_COUNT << lod; // in voxel space, ie. worldSize / VoxelSize
+
+		public OctreeCoord (int lod, int3 pos) {
+			this.lod = lod;
+			this.pos = pos;
+		}
+			
+		public override string ToString () {
+			return string.Format("({0}, ({1}, {2}, {3}))", lod, pos.x, pos.y, pos.z);
+		}
+		
+		// Needed to be used as Hashmap key
+		bool System.IEquatable<OctreeCoord>.Equals(OctreeCoord r) {
+			return lod == r.lod && all(pos == r.pos);
+		}
+		public override int GetHashCode () {
+			// https://stackoverflow.com/questions/1646807/quick-and-simple-hash-code-combinations
+			int h = 1009;
+			h = (h * 9176) + lod;
+			h = (h * 9176) + pos.GetHashCode();
+			return h;
+		}
+		
+		public static bool operator== (OctreeCoord l, OctreeCoord r) {
+			return l.lod == r.lod && all(l.pos == r.pos);
+		}
+		public static bool operator!= (OctreeCoord l, OctreeCoord r) {
+			return l.lod != r.lod || any(l.pos != r.pos);
+		}
+		
+		public override bool Equals (object obj) { // SLOW to call because it boxes OctreeCoord
+			if (obj == null || GetType() != obj.GetType())
+				return false;
+		
+			return this == (OctreeCoord)obj;
+		}
+	};
+	
+	/// <summary>
+	/// A Node in the terrain octree
+	/// Its location is immutable
+	/// Exists inside the octree, never outside
+	/// Is incomplete when created, since voxel and mesh creation are async, always gets created in the tree when the async ops are sheduled, since neighbourship queries are needed for the seam creation, which are really hard without a tree
+	/// Actual game object gets only created when as soon as the mesh is done
+	/// </summary>
 	public class TerrainNode {
 		public const int VOXEL_COUNT = 32;
 
 		 // TerrainNode has a fixed location after it is created
-		public readonly int Lod;
+		public readonly OctreeCoord Coord;
 		public readonly float3 Pos; // lower corner not center
 		public readonly float Size;
+
+		public int Lod => Coord.lod;
+		public float3 Center => Pos + Size/2;
 		
-		// this represents the Terrain Octree, TerrainNodes are also cached, so TerrainNodes can exits outside the tree, they should be disabled in that case
 		public TerrainNode[] Children = new TerrainNode[8];
 
-		public static readonly int3[] ChildOctants = new int3[8] { int3(0,0,0), int3(1,0,0),  int3(0,1,0), int3(1,1,0),   int3(0,0,1), int3(1,0,1),  int3(0,1,1), int3(1,1,1) };
-		public static readonly int3[] ChildDirs = new int3[8] { int3(-1,-1,-1), int3(+1,-1,-1),  int3(-1,+1,-1), int3(+1,+1,-1),   int3(-1,-1,+1), int3(+1,-1,+1),  int3(-1,+1,+1), int3(+1,+1,+1) };
 
-		public TerrainNode GetChild (int3 octant) { // [0,1]
-			return Children[ octant.z * 4 + octant.y * 2 + octant.x ];
-		}
-		
 		public GameObject Go;
 		public Mesh mesh = null;
 
@@ -50,6 +98,22 @@ namespace OctreeGeneration {
 
 		public Voxels Voxels;
 		public Cells  Cells;
+		
+		public static readonly int3[] ChildOctants = new int3[8] { int3(0,0,0), int3(1,0,0),  int3(0,1,0), int3(1,1,0),   int3(0,0,1), int3(1,0,1),  int3(0,1,1), int3(1,1,1) };
+		public static readonly int3[] ChildDirs = new int3[8] { int3(-1,-1,-1), int3(+1,-1,-1),  int3(-1,+1,-1), int3(+1,+1,-1),   int3(-1,-1,+1), int3(+1,-1,+1),  int3(-1,+1,+1), int3(+1,+1,+1) };
+		
+		public TerrainNode (OctreeCoord coord, float VoxelSize) {
+			this.Lod = lod;
+			this.Pos = pos;
+			this.Size = size;
+			
+			Go = Object.Instantiate(TerrainNodePrefab, pos, Quaternion.identity, goHierachy);
+			SeamGo = Go.transform.Find("Seam").gameObject;
+		}
+
+		public TerrainNode GetChild (int3 octant) { // [0,1]
+			return Children[ octant.z * 4 + octant.y * 2 + octant.x ];
+		}
 		
 		public static int GetChildrenMask (TerrainNode[] children) {
 			int mask = 0;
@@ -76,15 +140,6 @@ namespace OctreeGeneration {
 			var touching = new HashSet<TerrainNode>();
 			GetNodesInDir(this, dir, touching);
 			return touching;
-		} 
-
-		public TerrainNode (int lod, float3 pos, float size, GameObject TerrainNodePrefab, Transform goHierachy) {
-			this.Lod = lod;
-			this.Pos = pos;
-			this.Size = size;
-			
-			Go = Object.Instantiate(TerrainNodePrefab, pos, Quaternion.identity, goHierachy);
-			SeamGo = Go.transform.Find("Seam").gameObject;
 		}
 
 		public void Destroy () {
