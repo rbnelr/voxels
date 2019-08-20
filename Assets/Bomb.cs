a8fff800c90f59be8e2d3a72e403fe424d3572ea
Anon Ymous <rbnelr@gmx.de>
2019-08-20 23:22:13 +0200

    added explosion particle effects and physics force to bombs

--- Assets/Bomb.cs
+++ Assets/Bomb.cs
@@ -6,12 +6,27 @@ public class Bomb : MonoBehaviour {
 	public float FuseTime = 3f;
 
 	public float Radius = 4f;
+	public float ExplosionForce = 200f * 1000;
+
+	public GameObject Explosion;
 
 	private void Update () {
 		FuseTime -= Time.deltaTime;
 
 		if (FuseTime <= 0) {
-			VoxelEdit.SubstractSphere(transform.position, Radius);
+			if (Chunks.Instance != null)
+				VoxelEdit.SubstractSphere(transform.position, Radius);
+
+			{
+				var objs = Physics.OverlapSphere(transform.position, Radius*2);
+				for (int i=0; i<objs.Length; ++i) {
+					var rb = objs[i].GetComponent<Rigidbody>();
+					if (rb != null)
+						rb.AddExplosionForce(ExplosionForce, transform.position, Radius*2);
+				}
+			}
+
+			Instantiate(Explosion, transform.position, Quaternion.identity);
 
 			Destroy(this.gameObject);
 		}
---
+++ Assets/DeleteAfterTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteAfterTime : MonoBehaviour {
+	public float LifetimeRemain = 5f;
+
+	private void Update () {
+		LifetimeRemain -= Time.deltaTime;
+
+		if (LifetimeRemain <= 0)
+			Destroy(this.gameObject);
+	}
+}
